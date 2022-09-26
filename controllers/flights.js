8eb15f512da6e206aeec1cc1f44f6ddf8d9e1033
racssett <rachel.c.hassett@gmail.com>
2022-09-26 18:31:48 -0500

    Add full edit functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -58,10 +58,40 @@ function deleteFlight (req, res) {
   })
 }
 
+function edit (req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render('flights/edit', {
+      flight,
+      title: 'Edit Flight'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function update (req, res) {
+  for (let key in req.body){
+    if(req.body[key] === '') delete req.body[key]
+  }
+  Flight.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(flight => {
+    res.redirect(`/flights/${flight._id}`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 export {
   newFlight as new,
   create,
   index,
   show,
   deleteFlight as delete,
+  edit,
+  update,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -6,7 +6,9 @@ import * as flightsCtrl from '../controllers/flights.js'
 router.get('/new', flightsCtrl.new)
 router.get('/', flightsCtrl.index)
 router.get('/:id', flightsCtrl.show)
+router.get('/:id/edit', flightsCtrl.edit)
 router.post('/', flightsCtrl.create)
+router.put('/:id', flightsCtrl.update)
 router.delete('/:id', flightsCtrl.delete)
 
 export {
